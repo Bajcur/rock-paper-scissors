0e366ae9efce150da9d7326114129d5667d0ca9f
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-06-25 14:33:03 +0200

    Create playRound function
    Created playRound function is responsible for
    playing one round of the game. I takes user input
    and computer random choice of either rock, paper or
    scissors. Then it compares them and evaluates the
    result. It returns string with message, who has won
    the game, but doesn't print it itself.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 'use strict';
 
 
-const rps = ["Rock", "Paper", "Scissors"]
+const rps = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     let choice = rps[(Math.floor(Math.random() * rps.length))]
-    return console.log(choice)
+    return choice
 }
 
-getComputerChoice()
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Chose between Rock, Paper and Scissors!").toLowerCase();
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors!"                
+    } else if(playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lost! Rock loses to Paper!"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lost! Paper loses to Scissors!"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper!"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lost! Scissors loses to Rock!"
+    } else {
+        return "It's a Draw!"
+    }
+}
+
+
+
+
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
 
