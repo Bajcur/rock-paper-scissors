64cc9b254d1aa4fa46e02889603bc89e4321be1a
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-06-25 16:00:06 +0200

    Finish Rock Paper Scissors game
    This commit adds and changes multiple things.
    Changes return of playRound function to array, because
    we need an information who won a round. The array
    has a string which contains this information at 0 and
    string which might be shown to user at 1. Game function got
    created. It allows user to play 5 rounds against computer.
    It takes in loop playRound function as well as
    user prompt and computer generated output. It shows
    result of each round. It is capable of tracking user and
    computer score and displays a message at the end, who won
    the overall 5 round "Rock, Paper, Scissors" game.

--- script.js
+++ script.js
@@ -8,30 +8,65 @@ function getComputerChoice(){
     return choice
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Chose between Rock, Paper and Scissors!").toLowerCase();
+
+//const computerSelection = "scissors";
+//const playerSelection = prompt("Chose between Rock, Paper and Scissors!").toLowerCase();
 
 function playRound(playerSelection, computerSelection){
+    
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors!"                
+        let result = ["win", "You Win! Rock beats Scissors!"];
+        return result                
     } else if(playerSelection === "rock" && computerSelection === "paper"){
-        return "You Lost! Rock loses to Paper!"
+        let result = ["lost", "You Lose! Rock loses to Paper!"];
+        return result
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock!"
+        let result = ["win", "You Win! Paper beats Rock!"];
+        return result
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lost! Paper loses to Scissors!"
+        let result = ["lost", "You Lose! Paper loses to Scissors!"];
+        return result
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper!"
+        let result = ["win", "You Win! Scissors beats Paper!"];
+        return result
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lost! Scissors loses to Rock!"
+        let result = ["lost", "You Lose! Scissors loses to Rock!"];
+        return result
     } else {
-        return "It's a Draw!"
+        let result = ["draw", "It's a Draw!"];
+        return result
     }
 }
 
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i <= 4; i++){
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Chose between Rock, Paper and Scissors!").toLowerCase();
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result[1])
+        if (result[0] === "win"){
+            playerScore++;
+        } else if (result[0] === "lost") {
+            computerScore++;
+        } else {
+            computerScore++;
+            playerScore++;
+        }
+    }
+    console.log(playerScore);
+    console.log(computerScore);
+    if (playerScore > computerScore) {
+        return console.log("You' ve won the entire game!")
+    } else if (playerScore < computerScore) {
+        return console.log("You've lost the entire game! Better luck next time!")
+    } else {
+        return console.log("It's an overall Draw! Not great, not terrible!")
+    }
+}
 
-
-
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+game()
+//console.log(computerSelection);
+//console.log(playRound(playerSelection, computerSelection)[0]);
 
